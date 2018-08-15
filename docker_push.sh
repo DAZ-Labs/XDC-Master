e39b53a4e1f96b3861e0866a4f7e39c26b7ca37c
rahulraj04 <rahulrajnair94@gmail.com>
2018-08-15 19:48:04 +0530

    update .travis

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker tag XinFin/XDCmaster XinFin/XDCmaster:latest
-docker tag XinFin/XDCmaster XinFin/XDCmaster:$TRAVIS_BUILD_ID
-docker push XinFin/XDCmaster:latest
-docker push XinFin/XDCmaster:$TRAVIS_BUILD_ID
+docker tag XinFin/XDCmaster XinFin/XDCmaster:$1
+docker push XinFin/XDCmaster:$1
